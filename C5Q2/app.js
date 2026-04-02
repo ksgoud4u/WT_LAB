5487402407c265ea3ac4d0d5fd499357b0b630f7
ksgoud4u <23311a05u4@cse.sreenidhi.edu.in>
2026-04-02 13:52:41 +0530

    C5Q2
    c5q2

---
+++ C5Q2/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+app.use('/files', require('./routes/fileRoutes'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
